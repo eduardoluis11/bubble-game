d9f69948963f1a43c72b8b610186c9d005697be9
Eduardo Salinas <eduardoluis93@gmail.com>
2025-01-25 17:19:19 +0000

    (BUGGY) The player can now Jump, but it can jump infinitely.

--- Assets/Scripts/PlayerControllerForMovement.cs
+++ Assets/Scripts/PlayerControllerForMovement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerControllerForMovement : MonoBehaviour
 {
-    public Rigidbody2D rigidBody;
-    public float moveSpeed;
-    public float xInput;
+    private Rigidbody2D rigidBody;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private float xInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,5 +22,10 @@ public class PlayerControllerForMovement : MonoBehaviour
         xInput = Input.GetAxisRaw("Horizontal");
 
         rigidBody.velocity = new Vector2(xInput * moveSpeed, rigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
     }
 }
